d3d8397a33d5dfd9c9ceac21a2caab17aadb5807
Sabin Gaire <sabin.gaire@bison.howard.edu>
2020-02-13 15:28:20 -0500

    Syntax error

--- csci350.pl
+++ csci350.pl
@@ -1,4 +1,10 @@
-% Sabin Gaire
-% @02859054
-% This is the assignment for CSCI 350 class
-% This assignment is about Logic Programming.
+%Sabin Gaire
+%@02859054
+%This is the assignment for CSCI 350 class
+%This assignment is about Logic Programming.
+
+%The sum-up-numbers- simple will sum up the numbers
+
+sum-up-numbers-simple([], 0).
+
+
