9a2fdf9def0d4e00a81099604f7359b435bb3710
Sabin Gaire <sabin.gaire@bison.howard.edu>
2020-02-13 16:21:50 -0500

    Added the case when head is a number

--- csci350.pl
+++ csci350.pl
@@ -4,7 +4,14 @@
 %This assignment is about Logic Programming.
 
 %The sum-up-numbers- simple will sum up the numbers
+%If L=[], N=0
 
 sum-up-numbers-simple([], 0).
+%If head is number, then compute sum
+
+sum-up-numbers-simple([HEAD|TAIL], N):-
+number(HEAD),
+sum-up-numbers-simple(TAIL, TailSum),
+N is HEAD + TailSum.
 
 
